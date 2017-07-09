12c8d5f9356249831be2ff863fc8fdf74901893f
Morelatto <morelatto@protonmail.com>
2017-07-09 19:23:52 -0300

    Formatting.

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from scrapy import cmdline
+
 # cmdline.execute("scrapy crawl extra_delivery -o result_extra.json".split())
 cmdline.execute("scrapy crawl sonda_delivery -o result_sonda.json".split())
