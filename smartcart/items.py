56a11511c29d4dc1380e2d03b2d8d7500e5ffacb
Morelatto <morelatto@protonmail.com>
2017-07-09 22:21:32 -0300

    Added Pão de Açúcar spider Item and Loader. Extracted clean_sku method.

--- smartcart/items.py
+++ smartcart/items.py
@@ -2,9 +2,12 @@
 import scrapy
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, Compose, TakeFirst, Join
+from w3lib.html import remove_tags
 
 to_float = Compose(TakeFirst(), lambda v: v.replace('.', '').replace(',', '.'), float)
+to_int = Compose(TakeFirst(), int)
 clean_text = Compose(MapCompose(lambda txt: txt.strip()), Join())
+clean_sku = Compose(TakeFirst(), lambda url: url.split('/')[4], int)
 
 
 class Product(scrapy.Item):
@@ -22,7 +25,7 @@ class ProductLoader(ItemLoader):
     default_item_class = Product
     default_output_processor = TakeFirst()
 
-    sku_out = Compose(TakeFirst(), lambda url: url.split('/')[4], int)
+    sku_out = clean_sku
     name_out = clean_text
     price_out = to_float
     price_discount_out = to_float
@@ -38,3 +41,18 @@ class SondaDeliveryProductLoader(ProductLoader):
     default_item_class = SondaDeliveryProduct
 
     sub_category_out = clean_text
+
+
+class PaoDeAcucarProduct(Product):
+    brand = scrapy.Field()
+    description = scrapy.Field()
+
+
+class PaoDeAcucarProductLoader(ProductLoader):
+    default_item_class = PaoDeAcucarProduct
+
+    sku_out = to_int
+    price_out = TakeFirst()
+    price_discount_out = TakeFirst()
+    brand_out = clean_text
+    description_out = Compose(MapCompose(remove_tags), TakeFirst())
