7613feadaa608c0b0938d8ea981a8d56039aceff
Morelatto <morelatto@protonmail.com>
2017-07-09 22:23:18 -0300

    Added Pão de Açúcar spider run line.

--- main.py
+++ main.py
@@ -4,4 +4,5 @@
 from scrapy import cmdline
 
 # cmdline.execute("scrapy crawl extra_delivery -o result_extra.json".split())
-cmdline.execute("scrapy crawl sonda_delivery -o result_sonda.json".split())
+# cmdline.execute("scrapy crawl sonda_delivery -o result_sonda.json".split())
+cmdline.execute("scrapy crawl pao_de_acucar -o result_paodeacucar.json".split())
