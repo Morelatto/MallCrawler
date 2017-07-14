9586eaf0163dcd677de9bd2d1dc0a677a74dc38c
Morelatto <morelatto@protonmail.com>
2017-07-13 22:20:29 -0300

    Updated Extra SQL fields order to match items. Added SQL for Sonda and Pão de Açúcar.

--- sql/CREATE_SONDA_DELIVERY_PRODUCTS.sql
+++ sql/CREATE_SONDA_DELIVERY_PRODUCTS.sql
@@ -1,7 +1,7 @@
-CREATE TABLE EXTRA_DELIVERY_PRODUCTS
+CREATE TABLE SONDA_DELIVERY_PRODUCTS
 (
   guid           CHAR(32)  NOT NULL PRIMARY KEY,
-  CONSTRAINT EXTRA_DELIVERY_PRODUCTS_guid_uindex UNIQUE (guid),
+  CONSTRAINT SONDA_DELIVERY_PRODUCTS_guid_uindex UNIQUE (guid),
   sku            INT,
   `name`         TEXT,
   price          FLOAT,
@@ -10,7 +10,8 @@ CREATE TABLE EXTRA_DELIVERY_PRODUCTS
   image          TEXT,
   department     VARCHAR(255),
   category       VARCHAR(255),
-  `status`       TINYINT(1),
+  # `status`       TINYINT(1),
+  sub_category   VARCHAR(255),
   created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
