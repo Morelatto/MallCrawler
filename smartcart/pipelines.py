753d351b642b72f284ab93a5c847d29556bd8ebf
Morelatto <morelatto@protonmail.com>
2017-07-16 21:51:54 -0300

    Finished support for multiple items. Changed item_data object for tuple conversion. Replaced fixed query strings for extraction method. Added noinspection annotations.

--- smartcart/pipelines.py
+++ smartcart/pipelines.py
@@ -6,6 +6,10 @@ from smartcart.items import SondaDeliveryProduct, PaoDeAcucarProduct, ExtraDeliv
 from twisted.enterprise import adbapi
 
 
+def extract_field_names(item, extra=''):
+    return ''.join(['`{}`{}, '.format(key, extra) for key in item.keys()])[:-2]
+
+
 class ExtraDeliveryMySQLPipeline(object):
     """A pipeline to store the item in a MySQL database.
     This implementation uses Twisted's asynchronous database API.
@@ -42,57 +46,35 @@ class ExtraDeliveryMySQLPipeline(object):
         """Perform an insert or update."""
         guid = self._get_guid(item)
 
-        item_data = (
-            item.get('sku'),
-            item.get('name'),
-            item.get('price'),
-            item.get('price_discount'),
-            item.get('url'),
-            item.get('image'),
-            item.get('department'),
-            item.get('category'),
-        )
-
-        update_fields = 'sku=%s, `name`=%s, price=%s, price_discount=%s, url=%s, image=%s, department=%s, category=%s, '
-        insert_fields = 'sku, `name`, price, price_discount, url, image, department, category, '
-
         if type(item) is ExtraDeliveryProduct:
             table_name = 'EXTRA_DELIVERY_PRODUCTS'
-            item_data += (item.get('status'),)
-            update_fields += '`status`=%s'
-            insert_fields += '`status`'
-
         elif type(item) is SondaDeliveryProduct:
             table_name = 'SONDA_DELIVERY_PRODUCTS'
-            item_data += (item.get('sub_category'),)
-            update_fields += 'sub_category=%s'
-            insert_fields += 'sub_category'
-
         elif type(item) is PaoDeAcucarProduct:
             table_name = 'PAO_DE_ACUCAR_PRODUCTS'
-            item_data += (item.get('status'), item.get('brand'), item.get('description'),)
-            update_fields += '`status`=%s, brand=%s, description=%s'
-            insert_fields += '`status`, brand, description'
         else:
             raise DropItem('Invalid item class {}: {}'.format(type(item), item))
 
-        conn.execute("SELECT EXISTS(SELECT 1 FROM {} WHERE guid = %s)".format(table_name), (guid, ))
+        conn.execute("SELECT EXISTS(SELECT 1 FROM {} WHERE guid = %s)".format(table_name), (guid,))
 
         if conn.fetchone()[0]:
-            conn.execute("UPDATE {} SET {} WHERE guid=%s".format(table_name, update_fields), item_data + (guid,))
+            conn.execute("UPDATE {} SET {} WHERE guid=%s".format(table_name, extract_field_names(item)),
+                         tuple(item.values()) + (guid,))
             spider.logger.debug("Item updated in db: {} {}".format(guid, item))
         else:
-            conn.execute("INSERT INTO {} (guid, {}) VALUES ({} %s)".format(table_name, insert_fields,
-                                                                        (insert_fields.count(',') + 1) * '%s, '),
-                         (guid,) + item_data)
-            spider.logger.debug("Item stored in db: {} {}".format(guid, item))
+            conn.execute("INSERT INTO {} (guid, {}) VALUES ({})".format(table_name, extract_field_names(item),
+                                                                        extract_field_names(item, '=%s')),
+                         (guid,) + tuple(item.values()))
+            spider.logger.debug("Item inserted in db: {} {}".format(guid, item))
 
+    # noinspection PyMethodMayBeStatic
     def _handle_error(self, failure, item, spider):
         """Handle occurred on db interaction."""
         # do nothing, just log
         spider.logger.error("Item failed {}".format(item))
         spider.logger.error(failure)
 
+    # noinspection PyMethodMayBeStatic
     def _get_guid(self, item):
         """Generates an unique identifier for a given item."""
         # hash based solely in the url field
